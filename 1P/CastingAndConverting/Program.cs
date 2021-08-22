92d429d4597a953a1ab2dd1e2ea53366bde54f8d
Angel Brambila <malthael.91@gmail.com>
2021-08-22 13:08:08 -0500

    Class 04, Iteration Statements , Casting and Convertion

---
+++ 1P/CastingAndConverting/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+namespace CastingAndConverting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Casting
+            // Implicit Casting , Explicit Casting
+            int a = 10;
+            double b = a; // Implicit Casting,
+            WriteLine(b);
+            double c = 9.8;
+            int d = (int)c;
+            WriteLine(d);
+
+            long e = 5_000_000_000;
+            int f = (int)e;
+            WriteLine($"e is {e:N0} and f is {f}");
+            e = long.MaxValue;
+            f = (int)e;
+            WriteLine($"e is {e:N0} and f is {f:N0}");
+            #endregion
+
+            #region Convert
+                double g = 9.8;
+                int h = ToInt32(g);
+                WriteLine($"g is {g} and h is {h}");
+
+                double[] doub = new [] { 9.49, 9.5, 9.51, 10.49, 10.5, 10.51 };
+                foreach (double n in doub)
+                {
+                    WriteLine($"ToInt({n}) is {ToInt32(n)}");
+                }
+
+                foreach (double n in doub)
+                {
+                    WriteLine(format: "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+                        arg0: n,
+                        arg1: Math.Round(value: n, digits: 0, mode:MidpointRounding.AwayFromZero)
+                    );
+                }
+            #endregion
+
+            #region Converting TO String
+                int number = 12;
+                WriteLine(number.ToString());
+                bool boolean = true;
+                WriteLine(boolean.ToString());
+                object me = new object();
+                WriteLine(me.ToString());
+            #endregion
+
+            #region Binary to String
+                // 128 bytes
+                byte[] binaryObject = new byte[128];
+                (new Random()).NextBytes(binaryObject);
+                WriteLine("Binary Object as bytes: ");
+                for (int i = 0; i < binaryObject.Length; i++)
+                {
+                    Write($"{binaryObject[i]:X} ");
+                }
+                string encoded = ToBase64String(binaryObject);
+                WriteLine();
+                WriteLine($"Binary Object as Base64 : {encoded}");
+            #endregion
+
+            #region Parse
+                int age = int.Parse("30");
+                DateTime birthday = DateTime.Parse("2 August 1991");
+                WriteLine($"I was bors {age} years ago");
+                WriteLine($"My Birthday is {birthday}");
+                WriteLine($"My Birthday is {birthday:D}");
+            #endregion
+
+            #region Try Parse
+                Write("How many Zurdos are there? ");
+                int count;
+                string input = ReadLine();
+                if(int.TryParse(input, out count))
+                {
+                    WriteLine($"There are {count} Zurdos");
+                }
+                else
+                {
+                    WriteLine("I Could not parse the input");
+                }
+            #endregion
+        }
+    }
+}
---
+++ 1P/IterationStatements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region while
+                int x = 0;
+                while(x < 10)
+                {
+                    WriteLine(x);
+                    x++;
+                }
+            #endregion
+
+            #region do While
+                string password = string.Empty;
+                do
+                {
+                    Write("Enter password: ");
+                    password = ReadLine();
+                } while (password != "Pa$$w0rd");
+                WriteLine("BINGO!");
+            #endregion
+
+            #region for
+                for (int i = 0; i <= 10; i++)
+                {
+                    WriteLine(i);
+                }
+            #endregion
+
+            #region For Each
+               string[] names = {"Akali", "Zurdo", "Charlie"}; // IEnumerator, e.Next()
+               foreach (string name in names)
+               {
+                   WriteLine($"{name} has {name.Length} characters");
+               }
+            #endregion
+        }
+    }
+}
